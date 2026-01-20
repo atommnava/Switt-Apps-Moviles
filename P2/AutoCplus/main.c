00ed1cffef7ee4e08e551dd5bfa068331260ce2a
atommnava <atommunoznava@gmail.com>
2026-01-19 19:35:57 -0600

    Cambios

--- P2/AutoCplus/main.c
+++ P2/AutoCplus/main.c
@@ -6,9 +6,46 @@ typedef struct Auto {
     char marca[64];
     char modelo[64];
     int distancia;
-};
+} Auto;
+
+Auto *allocation(){
+    Auto *miAuto = NULL;
+    miAuto = (Auto*) malloc(sizeof(Auto));
+    return miAuto;
+
+}
+
+void initialization(Auto *miAuto, char *marca, char *modelo) {
+    strcpy(miAuto -> marca, marca);
+    strcpy(miAuto -> modelo, modelo);
+    miAuto -> distancia = 0;
+}
+
+void imprimir(Auto *miVocho){
+    printf("%s %s %d\n", miVocho -> marca, miVocho -> modelo, miVocho -> distancia);
+}
+
+void avanzar(Auto *miVocho, int x) {
+    miVocho -> distancia += x;
+}
 
 int main() {
-    printf("Hola")
+    Auto *miVocho = NULL;
+    miVocho = allocation();
+    initialization(miVocho, "Tesla", "S");
+    //miVocho = (Auto*) malloc(sizeof(Auto));
+    
+    avanzar(miVocho, 100);
+    imprimir(miVocho);
+
+    Auto *miFerrari = allocation();
+    initialization(miFerrari, "Ferrari", "Asotador");
+    avanzar(miFerrari, 500);
+    imprimir(miFerrari);
+    Auto *miCoche = NULL;
+    miCoche = miVocho;
+    imprimir(miCoche);
+    
+    printf("Hola");
     return 0;
 }
---
+++ P2/AutoCplus/main.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+int main()
+{
+    @autoreleasepool {
+        BOOL laQuiero = YES;
+        BOOL meQuiero = NO;
+        NSLog(@"Hola Mundo");
+        NSLog(@"Mi relación va a funcionar? : %d", laQuiero 66 meQuiero);
+    }
+    return 0;
+}
