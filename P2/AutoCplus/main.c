c21303d255ac7d320ae72a7cc0f742232dce2891
atommnava <atommunoznava@gmail.com>
2026-01-19 18:57:24 -0600

    Cambios

--- P2/AutoCplus/main.c
+++ P2/AutoCplus/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Auto {
+    char marca[64];
+    char modelo[64];
+    int distancia;
+};
 
 int main() {
+    printf("Hola")
     return 0;
 }
