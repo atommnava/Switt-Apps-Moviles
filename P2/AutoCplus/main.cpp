482bb2f07c06aa81817468ae92a3aeacb6258187
atommnava <atommunoznava@gmail.com>
2026-01-19 18:32:15 -0600

    Cambios

--- P2/AutoCplus/main.cpp
+++ P2/AutoCplus/main.cpp
@@ -2,9 +2,50 @@
 
 using namespace std;
 
+class Auto {
+    private:
+    string marca;
+    string modelo;
+    int distancia;
+
+    public:
+    Auto() {
+        marca = "";
+        modelo = "";
+        distancia = 0;
+    }
+    Auto(string marca, string modelo) {
+        this -> marca = marca;
+        this -> modelo = modelo;
+    }
+    Auto(string marca, string modelo, int distancia) {
+        this -> marca = marca;
+        this -> modelo = modelo;
+        this -> distancia = distancia;
+    }
+    void setMarca(string marca) {
+        this -> marca = marca;
+    }
+    void setModelo(string modelo) {
+        this -> modelo = modelo;
+    }
+    void setDistancia(int distancia) {
+        this -> distancia = distancia;
+    }
+    string getMarca(){
+        return marca;
+    }
+    string getModelo(){
+        return modelo;
+    }
+    int getDistancia(){
+        return distancia;
+    }
+};
+
 int main()
 {
-    cout << "Hola Mundo!";
+    cout << "Hola Mundo!" << endl;
 
     return 0;
 }
